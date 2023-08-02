c51f1e466b3c8505e7b5b891cd9331c612e57f52
JitendraRay <jkjohnson552@gmail.com>
2023-08-02 15:43:21 +0530

    update delet done

--- std_manage/std/urls.py
+++ std_manage/std/urls.py
@@ -3,5 +3,9 @@ from .views import *
 urlpatterns = [
     path("",home),
     path("home/",home),
-    path("add-std",std_add)
+    path("add-std",std_add),
+    path("delete-std/<int:roll>",delete_std),
+    path("edit-std/<int:roll>",update_std),
+    path("do_edit-std/<int:roll>",do_update_std),
+
 ]
--- std_manage/std/views.py
+++ std_manage/std/views.py
@@ -30,3 +30,31 @@ def std_add(request):
         return redirect("/")
 
     return render(request,"std/add_std.html",{})
+
+def delete_std(request,roll):
+    s=Student.objects.get(pk=roll)
+    s.delete()
+    return redirect("/")
+
+
+def update_std(request,roll):
+    std=Student.objects.get(pk=roll)
+    return render(request,"std/update_std.html",{'std':std})
+
+def do_update_std(request,roll):
+     std_roll=request.POST.get("std_roll")
+     std_name=request.POST.get("std_name")
+     std_email=request.POST.get("std_email")
+     std_contact=request.POST.get("std_contact")
+     std_address=request.POST.get("std_address")
+
+     std = Student.objects.get(pk=roll)
+
+     std.roll=std_roll
+     std.name=std_name
+     std.email=std_email
+     std.contact=std_contact
+     std.address=std_address 
+
+     std.save()
+     return redirect("/")
